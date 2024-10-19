c6d9c957def93c367f0f68a554739bb93522c1d0
Hamza <hh0718@yahoo.com>
2024-10-18 23:10:38 -1000

    more work on battle screen

--- Battle.gd
+++ Battle.gd
@@ -3,19 +3,41 @@ extends CanvasLayer
 @onready var level_label = $"Control/Level Label"
 @onready var hp_label = $"Control/HP Label"
 @onready var mp_label = $"Control/MP Label"
-@onready var exp_label = $"Control/EXP Label"
-@onready var gold_label = $"Control/Gold Label"
 
+var isPlayerTurn: bool = false
+var rng = RandomNumberGenerator.new()
+
+var giant_rat = {
+  "name": "giant_rat",
+  "attack": 10,
+  "defense": 10,
+  "speed": 10,
+  "max_hp": 10,
+  "max_mp": 0
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	level_label.text = "Level: " + str(PlayerAttributes.level)
 	hp_label.text = "HP: " + str(PlayerAttributes.currentHP) + "/" + str(PlayerAttributes.maxHP)
 	mp_label.text = "MP: " + str(PlayerAttributes.currentMP) + "/" + str(PlayerAttributes.maxMP)
-	exp_label.text = "EXP: " + str(PlayerAttributes.exp)
-	gold_label.text = "Gold: " + str(PlayerAttributes.gold)
-
+	
+	decideTurnOrder()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+func _on_attack_button_pressed():
+	decideTurnOrder()
+	print(giant_rat["attack"])
+	
+func decideTurnOrder():
+	var player_speed = getRandNumRange(0.1, PlayerAttributes.speed)
+	var enemy_speed = getRandNumRange(0.1, giant_rat.speed)
+	if(player_speed > enemy_speed):
+		isPlayerTurn = true
+	print("players turn?: " + str(isPlayerTurn))
+	
+func getRandNumRange(percent: float, num: int):
+	return rng.randi_range(num - percent*num, num + percent*num)
--- player_attributes.gd
+++ player_attributes.gd
@@ -1,12 +1,20 @@
 extends Node
 
 var level: int = 1
-var strength: int = 5
-var agility: int = 5
+
+var strength: int = 10
+var attack: int = 10
+
+var evasion: int = 10
+var defense: int = 10
+
+var speed: int = 10
+
 var maxHP: int = 20
 var maxMP: int = 20
-var currentHP: int = 10
-var currentMP: int = 10
-var exp: int = 100
-var luck: int = 10
-var gold: int = 5
+
+var currentHP: int = 20
+var currentMP: int = 20
+
+var experience: int = 0
+var gold: int = 0
